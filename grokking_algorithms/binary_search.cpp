e41f6cb4217ad656ad218527c463d436dc6ade00
alejandrorv68 <31384598+alejandrorv68@users.noreply.github.com>
2020-03-17 14:47:41 +0100

    update binary search

--- grokking_algorithms/binary_search.cpp
+++ grokking_algorithms/binary_search.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
 
+
+//template <class genlist>
+
+int binary_search(double *list, double value){
+	int low = 0;
+	int high = sizeof(list);
+	return high;
+}
+
+int main(){
+
+	double search_in[5] = {0.0,0.0,2.0,0.0,0.0};
+	double desired_val = 3.4;
+	//int result = binary_search(search_in,desired_val);
+	int result = sizeof(&search_in);
+	std::cout<<result<<std::endl;
+
+
+	return 0;
+}
